b98ff34faf55766ef2d85c23e7156f161ebdc7db
jean <evgenijpocekutov6@gmail.com>
2026-04-28 10:59:53 +0700

    Added possibility of marking a word and the choose of file

--- game.py
+++ game.py
@@ -4,7 +4,14 @@ import re
 from typing import Dict
 import random
 import time
+import os
 
+
+# Можно добавить вывод статистики
+
+# Можно добавить возможность пополнеиня словаря
+
+# Добавить в игру отмечание выученных слов
 
 def create_dict(file_path: str) -> Dict:
     eng_dict = dict()
@@ -26,10 +33,10 @@ def choose_mode() -> int:
 
     while True:
         try:
-            mode = int(input("Choose The Game Mode: "))
+            mode = int(input("Choose The Game Mode: ").strip())
 
             if mode not in [1, 2]:
-                print("Please choose the number 1 or 2")
+                print("Please, enter the number 1 or 2")
                 continue
             return mode
         except:
@@ -38,31 +45,33 @@ def choose_mode() -> int:
 
 def choose_level() -> float:
     levels = {
-            1: 0.5,
-            2: 1.0,
+            1: 0.0,
+            2: 4.0,
             3: 2.0,
-            4: 3.0
+            4: 1.0,
+            5: 0.5
     }
 
     print("\nThere is 3 Levels")
-    print(" 4 - easy")
+    print(" 1 - controlled")
+    print(" 2 - easy")
     print(" 3 - medium")
-    print(" 2 - hard")
-    print(" 1 - english God")
+    print(" 4 - hard")
+    print(" 5 - english God")
 
     while True:
         try:
-            level = int(input("Choose The Game Level: "))
+            level = int(input("Choose The Game Level: ").strip())
 
-            if level not in list(range(1, 5)):
-                print("Please enter the number 1, 2, 3 or 4")
+            if level not in list(range(1, 6)):
+                print("Please, enter the number 1, 2, 3, 4 or 5")
                 continue
             return levels[level]
         except:
             raise ValueError("Unknown level for Game")
 
 
-def run_game(mode: int, speed: float, eng_dict: Dict) -> None:
+def run_game(mode: int, speed: float, eng_dict: Dict, filepath: str) -> None:
 
     print("\nStart The Process")
     print(20 * "-")
@@ -82,13 +91,21 @@ def run_game(mode: int, speed: float, eng_dict: Dict) -> None:
         if mode == 2:
             word, translate = translate, word
         
-        print(f"[{index + 1}] Word: ", word, sep=" ", end="")
-        #time.sleep(speed)
-        symbol = input()
-        if symbol == '':
-            result += 1
-        print("Translate: ", translate, end="\n\n")
-        #time.sleep(1)
+        print(f"\n[{index + 1}] Word: ", word)
+        time.sleep(speed) 
+        if speed == 0.0:
+            symbol = input().strip()
+            if symbol == '':
+                mark_known(filepath, index + 1, True)
+                result += 1
+            elif symbol == 'q':
+                print(" == Exit ==")
+                exit(1)
+            else:
+                mark_known(filepath, index + 1, False)
+
+        print("Translate: ", translate)
+        time.sleep(1.0)
 
         if len(used) == len(eng_dict):
             print("\n == Game Over ==")
@@ -99,16 +116,55 @@ def run_game(mode: int, speed: float, eng_dict: Dict) -> None:
             print(f" == {len(used) * 100 // len(eng_dict)}% completed ==\n")
 
 
+def mark_known(filepath: str, number: int, known: bool):
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    if known:
+        new_content = content.replace(f"{number}. **", f"{number}. 🔥**")
+    else:
+        new_content = content.replace(f"{number}. 🔥**", f"{number}. **")
+
+    with open(filepath, "w") as file:
+        file.write(new_content)
+
+
+def choose_file(folder_path: str) -> str:
+
+    print("\n == All Awailable Dictionaries ==\n")
+
+    paths = []
+    num = 1
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            print(f"{num}. {file}")
+            paths.append(os.path.join(root, file))
+            num += 1
+
+    try:
+        while True:
+            index_file = int(input(" Choose The File's Number: ").strip())
+            if index_file not in list(range(1, num)):
+                print(f"Please, enter the number from the range {list(range(1, num))}")
+                continue
+            return paths[index_file - 1]
+    except:
+        raise ValueError("Unknown file's number")
+
+
 if __name__ == "__main__":
     
     print(" === The English Game ===\n")
 
     try:
-        path = "../../../../mnt/c/Users/user/Job/Smth/Backend/English/Dictionaries/The-Curious-Incident-of-the-Dog-in-the-Night-Time.md"
-        eng_dict = create_dict(path)
+        path = "../../../../mnt/c/Users/user/Job/Smth/Backend/English/Dictionaries"
+
+        filepath = choose_file(path)
+
+        eng_dict = create_dict(filepath)
 
         mode = choose_mode()
         speed = choose_level()
-        run_game(mode, speed, eng_dict)
+        run_game(mode, speed, eng_dict, filepath)
     except Exception as e:
         print(f"Error: {e}")
